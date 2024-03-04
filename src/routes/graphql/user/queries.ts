d07f70c7c16f0f6a4875820affa23aab93c2d3d3
Sadullo <sadulloburiyev1992@gmail.com>
2024-03-05 01:55:30 +0500

    feat: completed test-loader-prime

--- src/routes/graphql/user/queries.ts
+++ src/routes/graphql/user/queries.ts
@@ -1,4 +1,9 @@
 import { GraphQLList, GraphQLNonNull } from 'graphql';
+import {
+  parseResolveInfo,
+  ResolveTree,
+  simplifyParsedResolveInfoFragmentWithType,
+} from 'graphql-parse-resolve-info';
 import { IContext } from '../types/types.js';
 import { UUIDType } from '../types/uuid.js';
 import { userType } from './schemas.js';
@@ -6,8 +11,44 @@ import { userType } from './schemas.js';
 export const userQueries = {
   users: {
     type: new GraphQLList(userType),
-    resolve: (_, _args, { prisma }: IContext) => prisma.user.findMany(),
+    resolve: async (_, _args, { prisma, loader }: IContext, resolveInfo) => {
+      const parsedResolveInfoFragment = parseResolveInfo(resolveInfo) as ResolveTree;
+      const { fields } = simplifyParsedResolveInfoFragmentWithType(
+        parsedResolveInfoFragment,
+        resolveInfo.returnType,
+      );
+
+      const hasUserSubscribedTo = fields.hasOwnProperty('userSubscribedTo');
+      const hasSubscribedToUser = fields.hasOwnProperty('subscribedToUser');
+
+      const users = await prisma.user.findMany({
+        include: {
+          userSubscribedTo: hasUserSubscribedTo,
+          subscribedToUser: hasSubscribedToUser,
+        },
+      });
+
+      if (hasUserSubscribedTo || hasSubscribedToUser) {
+        users.forEach((user, _, arr) => {
+          if (hasUserSubscribedTo) {
+            const subscribers = user.userSubscribedTo.map(({ authorId }) =>
+              arr.find((item) => item.id === authorId),
+            );
+            loader.userSubscribedToLoader.prime(user.id, subscribers);
+          }
+          if (hasSubscribedToUser) {
+            const subscribers = user.subscribedToUser.map(({ subscriberId }) =>
+              arr.find((item) => item.id === subscriberId),
+            );
+            loader.subscribedToUserLoader.prime(user.id, subscribers);
+          }
+        });
+      }
+
+      return users;
+    },
   },
+
   user: {
     type: userType,
     args: {
