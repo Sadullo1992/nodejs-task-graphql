5e3d45febe7a75e7ecdbe69fcf9440d6a8ae1ade
Sadullo <sadulloburiyev1992@gmail.com>
2024-11-04 11:31:52 +0500

    feat: add profile queries & loader

---
+++ src/routes/graphql/profile/schemas.ts
@@ -0,0 +1,74 @@
+import {
+  GraphQLBoolean,
+  GraphQLInputObjectType,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+} from 'graphql';
+import { memberType, memberTypeId } from '../member/schemas.js';
+import { IContext } from '../types/types.js';
+import { UUIDType } from '../types/uuid.js';
+
+export const profileType = new GraphQLObjectType({
+  name: 'ProfileType',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(UUIDType),
+      description: 'ID',
+    },
+    isMale: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+      description: 'gender',
+    },
+    yearOfBirth: {
+      type: new GraphQLNonNull(GraphQLInt),
+      description: 'content of post',
+    },
+    userId: {
+      type: new GraphQLNonNull(UUIDType),
+      description: 'user ID',
+    },
+    memberTypeId: {
+      type: memberTypeId,
+      description: 'member type ID',
+    },
+    memberType: {
+      type: memberType,
+      resolve: async (args, _, { loader }: IContext) =>
+        await loader.memberTypeLoader.load(args.memberTypeId),
+    },
+  }),
+});
+
+export const profileInputType = new GraphQLInputObjectType({
+  name: 'CreateProfileInput',
+  fields: {
+    isMale: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+    },
+    yearOfBirth: {
+      type: new GraphQLNonNull(GraphQLInt),
+    },
+    userId: {
+      type: new GraphQLNonNull(UUIDType),
+    },
+    memberTypeId: {
+      type: memberTypeId,
+    },
+  },
+});
+
+export const profileChangeInputType = new GraphQLInputObjectType({
+  name: 'ChangeProfileInput',
+  fields: {
+    isMale: {
+      type: GraphQLBoolean,
+    },
+    yearOfBirth: {
+      type: GraphQLInt,
+    },
+    memberTypeId: {
+      type: memberTypeId,
+    },
+  },
+});
